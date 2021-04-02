eb4979f2a133e0f17dca7c223ee9a7dbf74f7a35
Alejandro Cáceres <alejandro.caceres@wardenautomation.com>
2021-04-01 20:52:45 -0400

    Adding budget state

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,35 @@
-import react, { Fragment } from "react";
+import react, { Fragment, useState } from "react";
 
 const Question = () => {
+  // defining states
+  const  [qty, setQty] = useState(0);
+
+  // change handler
+  const defineBudget = e => {
+    setQty(parseInt(e.target.value));
+  }
+
+  // on submit
+  const addBudget = e => {
+      e.preventDefault();
+
+      // validation
+
+      // on validation success
+  }
 
     return (
         <Fragment>
             <h2>Add your budget</h2>
 
-            <form>
+            <form
+                onSubmit={addBudget}
+            >
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Your badget"
+                    onChange={defineBudget}
                 />
                 <input
                     type="submit"
